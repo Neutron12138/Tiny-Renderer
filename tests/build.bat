de7946a2ec5a595526e8a9e60453ca1a4e6b1637
Neutron12138 <121910770+Neutron12138@users.noreply.github.com>
2023-09-09 12:57:58 +0800

    Delete tests directory

--- tests/build.bat
+++
@@ -1,3 +0,0 @@
-g++ -finput-charset=UTF-8 -fexec-charset=gbk -shared ^
-    "../src/TR.cpp" -o "TR.dll" ^
-    -lsfml-graphics -lsfml-system -lsfml-window -lopengl32 -lgdi32 -lglew32
--- tests/run.bat
+++
@@ -1,5 +0,0 @@
-set filename=base
-g++ -finput-charset=UTF-8 -fexec-charset=gbk ^
-    "../src/Main.cpp" "%filename%.cpp" "TR.dll" -o "%filename%.exe" ^
-    -lsfml-graphics -lsfml-system -lsfml-window -lopengl32 -lgdi32 -lglew32
-"%filename%.exe"
